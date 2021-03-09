f125322d41004a4dd6f62e26cb507360cb941201
Gnoss-song <silmxmail@naver.com>
2021-03-09 17:59:54 +0900

    1

--- [1]git/PythonWorkspace/practice.py
+++
@@ -1,9 +0,0 @@
-absent =[2,5]
-no_book = [7]
-for student in range(1,11):
-    if student in absent:
-        continue
-    elif student in no_book:
-        print("{0}, 수업 여기까지야.".format(student))
-        break
-    print("{0}, 책 읽어봐".format (student))
