6ceca781f84d6aa69d4a2bf510a3147b60ba31d4
Gnoss-song <79887378+Gnoss-song@users.noreply.github.com>
2021-03-31 22:49:59 +0900

    Delete 금요일 수업 directory

--- 금요일 수업/코틀린/SK-T-Academy/kotlinBook/src/KotlinJoinToString.kt
+++
@@ -1,5 +0,0 @@
-fun main(args:Array<String>){
-    var result = listOf(1,2,3,4,5,6,7,8)
-    var printResult = result.joinToString(separator = "|",prefix = "Int값 ",postfix = " 의 평균은 => ")
-    println("$printResult ${result.average()}")
-}
